3bc2dce2f9f42e7cebb01d863ca1862666f93c33
Jordan L Siaha <jordansiaha@hardiment.cs.arizona.edu>
2017-09-11 13:35:47 -0700

    khlaflksad

--- Hello.java
+++ Hello.java
@@ -15,6 +15,8 @@ public class Hello {
     }
 
     public static void goldLeader() {
+        public static void main (String args []) {
+            System.out.println ("Loosen up!")}
 
         // Gold Leader should add a print statement that says "Loosen up!"
     
@@ -23,7 +25,7 @@ public class Hello {
 
     public static void redLeader() {
         public static void main (String args []) {
-		System.out.println ("I copy, gold leader!")}
+            System.out.println ("I copy, gold leader!")}
         // Red Leader should add a print statement that says "I copy, gold leader!"
         
 
