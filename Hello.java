10767f5668b11d26d8ee713ff126dfd7e09e8423
Shu Lin <shulin@email.arizona.edu>
2017-09-11 13:31:05 -0700

    ...

--- Hello.java
+++ Hello.java
@@ -9,8 +9,6 @@ public class Hello {
     }
 
     public static void callSign() {
-        public static void main (String args []) {
-		System.out.println ("Lock S-foils in attack position.")}
         // Red Leader should add a print statement that says "Lock S-foils in attack position." 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
@@ -24,7 +22,8 @@ public class Hello {
     }
 
     public static void redLeader() {
- 
+        public static void main (String args []) {
+		System.out.println ("I copy, gold leader!")}
         // Red Leader should add a print statement that says "I copy, gold leader!"
         
 
